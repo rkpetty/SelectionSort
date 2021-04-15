49590dddf9255bbff9a944326a3a8ffce3065652
rkpetty <rkpetty@umich.edu>
2021-04-15 18:06:02 -0400

    fixed merge leftovers

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -2,29 +2,20 @@
 public class SelectionSort {
 	private int temp;
 	/** Creates a new instance of SelectionSort */
-	public SelectionSort() { }//constructor    
-		
+	public SelectionSort() { }//constructor
+
 	public int[] basicSelectionSort(int[] x) {
 		for (int i = 0; i < x.length; ++i) {
 			for (int j= i+1; j < x.length; ++j) {
 				if (x[i] > x[j]) {
 					temp = x[i];
 					x[i] = x[j];
-<<<<<<< HEAD
-<<<<<<< HEAD
-					temp = x[j];
-					
-=======
 					x[j] = temp; //fix
->>>>>>> MixedDuplicates
-=======
-					x[j] = temp; //fix
->>>>>>> PositiveNegative
-					}          
-				} // end of inner for loop      
+
+					}
+				} // end of inner for loop
 			} // end of outer for loop
-		return x;  
-		
+		return x;
+
 	} // end of basicSelectionSort method}
 }
-
--- src/TestSelectionSort.java
+++ src/TestSelectionSort.java
@@ -7,55 +7,44 @@ import junit.framework.TestCase;
 
 public class TestSelectionSort extends TestCase{
 	@Test public void test() {
-<<<<<<< HEAD
-		//testPositive();
-		//testNegative();
-		testMixed();
-		testDuplicates();
-=======
 		testPositive();
 		testNegative();
-		//testMixed();
-		//testDuplicates();
->>>>>>> PositiveNegative
-		}    
-	public TestSelectionSort() { }    
-	
-	public void testPositive(){   
-		
-	}    
-	
-	public void testNegative(){      
-		
-		/** Test data contains negative values only **/    
-		
-	}
-	
-	public void testMixed(){
-		
-    /** Test data contains with both positive, negative and zeros **/
-<<<<<<< HEAD
-=======
-		int[] arr = new int[5];        
-		arr[0] = -8;        
-		arr[1] = 9;        
-		arr[2] = 7;        
-		arr[3] = -10;        
-		arr[4] = -2;        
-		int[] Sortedarr = new int[5];        
-		Sortedarr[2] = -2;        
-		Sortedarr[3] = 7;
-		Sortedarr[1] = -8;  
-		Sortedarr[4] = 9;     
-		Sortedarr[0] = -10;
->>>>>>> MixedDuplicates
-		
-		}    
-	
-	public void testDuplicates(){   
-		
-		
-	}
-	
-}
+		testMixed();
+		testDuplicates();
+		}
+	public TestSelectionSort() { }
 
+	public void testPositive(){
+
+	}
+
+	public void testNegative(){
+
+		/** Test data contains negative values only **/
+
+	}
+
+	public void testMixed(){
+
+    /** Test data contains with both positive, negative and zeros **/
+		int[] arr = new int[5];
+		arr[0] = -8;
+		arr[1] = 9;
+		arr[2] = 7;
+		arr[3] = -10;
+		arr[4] = -2;
+		int[] Sortedarr = new int[5];
+		Sortedarr[2] = -2;
+		Sortedarr[3] = 7;
+		Sortedarr[1] = -8;
+		Sortedarr[4] = 9;
+		Sortedarr[0] = -10;
+
+		}
+
+	public void testDuplicates(){
+
+
+	}
+
+}
