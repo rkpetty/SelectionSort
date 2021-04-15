b45527ea95e367414bdb13825fbeb792d4b1839a
rkpetty <rkpetty@umich.edu>
2021-04-15 16:33:56 -0400

    Initial commit

---
+++ src/SelectionSort.java
@@ -0,0 +1,22 @@
+
+public class SelectionSort {
+	private int temp;
+	/** Creates a new instance of SelectionSort */
+	public SelectionSort() { }//constructor    
+		
+	public int[] basicSelectionSort(int[] x) {
+		for (int i = 0; i < x.length; ++i) {
+			for (int j= i+1; j < x.length; ++j) {
+				if (x[i] > x[j]) {
+					temp = x[i];
+					x[i] = x[j];
+					temp = x[j];
+					//x[j] = temp; //fix
+					}          
+				} // end of inner for loop      
+			} // end of outer for loop
+		return x;  
+		
+	} // end of basicSelectionSort method}
+}
+
